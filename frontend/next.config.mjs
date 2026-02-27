8367dded417bc4dd267d1898c3c15fcebd1b4682
MSzgy <04141257@cumt.edu.cn>
2026-02-27 15:09:11 +0800

    chore: Add `allowedDevOrigins` and disable API proxy rewrites.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,15 +7,15 @@ const nextConfig = {
     unoptimized: true,
   },
   allowedDevOrigins: ["capcut.mosuyang.org"],  // ← 加这一行
-  async rewrites() {
-    return [
-      {
-        // 凡是访问 /api/proxy/xxx 的请求，都由 Next.js 转发给 1111 端口后端
-        source: '/api/proxy/:path*',
-        destination: 'https://capcut.mosuyang.org/api/:path*',
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       // 凡是访问 /api/proxy/xxx 的请求，都由 Next.js 转发给 1111 端口后端
+  //       source: '/api/proxy/:path*',
+  //       destination: 'https://capcut.mosuyang.org/api/:path*',
+  //     },
+  //   ]
+  // },
   experimental: {
     serverActions: {
       // 如果你使用了 Server Actions 并且报错，也可以在这里配置
